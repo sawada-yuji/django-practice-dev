e1b018c3af7a0f85267530ae8d2864312353b7b9
sawada-yuji <sawadayuji@hotmail.com>
2026-03-12 16:34:07 +0900

    regular update 20260312

--- app/project3/bookproject/book/admin.py
+++ app/project3/bookproject/book/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import SampleModel
+#from .models import SampleModel
 
-admin.site.register(SampleModel)
+#admin.site.register(SampleModel)
 # Register your models here.
+
+from .models import Book
+admin.site.register(Book)
--- app/project3/bookproject/book/models.py
+++ app/project3/bookproject/book/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class SampleModel(models.Model):
+#class SampleModel(models.Model):
+#    title = models.CharField(max_length=100)
+#    number = models.IntegerField()
+
+CATEGORY = (('business', 'ビジネス'), ('life', '生活'), ('other', 'その他'))
+
+class Book(models.Model):
     title = models.CharField(max_length=100)
-    number = models.IntegerField()
-    
+    text = models.TextField()
+    category = models.CharField(
+        max_length = 100,
+        choices = CATEGORY
+    )
+
+    def __str__(self):
+        return self.title
+        
